a9e317e58a59dfc0f315a294be78d4e775758b1c
Lucia <luciiabz.465@gmail.com>
2024-09-07 12:04:02 -0300

    add class Alumno and Materia

--- system/src/Models/Alumno.java
+++ system/src/Models/Alumno.java
@@ -1,4 +1,36 @@
 package Models;
 
-public class Alumno {
-}
+import java.util.ArrayList;
+import java.util.List;
+
+
+    public class Alumno {
+
+        //atributos
+        private String nombre;
+        private String apellido;
+        private String dni;
+        private int NroLegajo;
+        private String carrera;
+        private List<Materia> materiasInscriptas;
+
+
+        //constructor (siempre debe llamarse como la clase)
+        public Alumno() {
+            this.materiasInscriptas = new ArrayList<>();
+        }
+
+
+        //metodos
+        public void setCarrera(String carrera) //set introduce dato
+        {
+            this.carrera = carrera;
+        }
+
+
+        public String getCarrera() { //get presenta datp
+            return carrera;
+        }
+
+    }
+
--- system/src/Models/Materia.java
+++ system/src/Models/Materia.java
@@ -1,4 +1,29 @@
 package Models;
 
+
 public class Materia {
+    private String nombre;
+    private int duracion;
+    private String profesor;
+    private String carrera;
+    private String horario;
+    private String aula;
+    private int cantidadHoras;
+
+
+    //constructor
+    public Materia(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+    //metodos
+    public String getNombre() {
+        return nombre;
+    }
+
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }

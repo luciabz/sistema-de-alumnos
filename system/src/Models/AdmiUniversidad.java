70d40f7fdaf9b2a7418d8abeaaab0c178f2ef3af
Lucia <luciiabz.465@gmail.com>
2024-09-08 19:40:57 -0300

    solucion errores admi

--- system/src/Models/AdmiUniversidad.java
+++ system/src/Models/AdmiUniversidad.java
@@ -1,32 +1,42 @@
 package Models;
-
 import java.util.ArrayList;
 import java.util.List;
 
     public class AdmiUniversidad {
-        private List<Carrera> carreras = new ArrayList<>();
-        private List<Profesor> profesores = new ArrayList<>();
-        private List<Alumno> alumnos = new ArrayList<>();
+        private List<Carrera> listasCarreras = new ArrayList<>();
+        private List<Profesor> listaProfesores = new ArrayList<>();
+        private List<Alumno> alumnosMatriculados = new ArrayList<>();
+        private List<Materia> listaMaterias = new ArrayList<>();
+        private List<Coordinador> listaCoordinadores = new ArrayList<>();
 
-
-        public AdmiUniversidad(List<Materia> materiasInscriptas, List<Carrera> carreras, List<Alumno> Alumnos) {
-            this.listasMaterias = new ArrayList<>();
+        public AdmiUniversidad(List<Materia> listasMaterias, List<Carrera> listasCarreras, List<Alumno> alumnosMatriculados, List<Profesor> listaProfesores, List<Coordinador> listaCoordinadores) {
+            this.listaMaterias = new ArrayList<>();
             this.listasCarreras = new ArrayList<>();
-            this.alumnosInscriptos = new ArrayList<>();
-
+            this.alumnosMatriculados = new ArrayList<>();
+            this.listaProfesores = new ArrayList<>();
+            this.listaCoordinadores = new ArrayList<>();
         }
 
+
+        public List<Coordinador> getListaCoordinadores() {
+            return listaCoordinadores;
+        }
+
+        public List<Profesor> getProfesores() {
+            return listaProfesores;
+        }
         public List<Materia> getListasMaterias() {
-            return materia;
+            return listaMaterias;
         }
 
         public List<Carrera> getListasCarreras() {
-            return carreras;
+            return listasCarreras;
         }
 
-        public List<Alumno> getAlumnosInscriptos() {
-            return Alumnos;
+        public List<Alumno> getAlumnosMatriculados() {
+            return alumnosMatriculados;
         }
+
     }
 
 
